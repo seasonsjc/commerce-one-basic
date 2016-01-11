3813ace42223118c958ed23a4194eff2b05331d0
ercarval <ercarval@gmail.com>
2016-01-11 19:51:07 -0200

    initial import

---
+++ src/com/commerceone/identity/authentication/AuthService.java
@@ -0,0 +1,43 @@
+package com.commerceone.identity.authentication;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AuthService
+ */
+@WebServlet( "/AuthService")
+public class AuthService extends HttpServlet {
+	       
+	private static final long serialVersionUID = -5417404638032003031L;
+
+	
+	
+	public AuthService() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request
+						, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter()
+				.append("<html>")
+				.append("<body>")
+				.append(request.getContextPath())
+				.append("</body>")
+				.append("</html>");
+				
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
